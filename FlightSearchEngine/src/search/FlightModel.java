5e138f83a53f2491c07beacf04d447b027411262
PeturLogi <plp2@hi.is>
2017-04-23 17:05:24 +0000

    Reyndi að laga fjölda sæta

--- FlightSearchEngine/src/search/FlightModel.java
+++ FlightSearchEngine/src/search/FlightModel.java
@@ -14,8 +14,11 @@ public class FlightModel implements Comparator{
 	private String arrivaltime;
 	private int seats;
 	private int price;
-	private Seats flightseats;
-	
+
+	private int avalibleSeats;
+	private int firstClass;
+	private int economyClass;
+
 	public FlightModel(int flightnumber, String departure,
 			String arrival, String departuredate, String departuretime,
 			String arrivaldate, String arrivaltime, int seats, int price) {
@@ -28,89 +31,117 @@ public class FlightModel implements Comparator{
 		this.arrivaltime = arrivaltime;
 		this.seats = seats;
 		this.price = price;
-		
-		this.flightseats = new Seats(seats);
+
+		this.avalibleSeats = seats;
+		this.firstClass = 20;
+		this.economyClass = this.avalibleSeats - this.firstClass;
 	}
-	
+
 	public int getFlightnumber() {
 		return flightnumber;
 	}
-	
+
 	public void setFlightnumber(int flightnumber) {
 		this.flightnumber = flightnumber;
 	}
-	
+
 	public String getDeparture() {
 		return departure;
 	}
-	
+
 	public void setDeparture(String departure) {
 		this.departure = departure;
 	}
-	
+
 	public String getArrival() {
 		return arrival;
 	}
-	
+
 	public void setArrival(String arrival) {
 		this.arrival = arrival;
 	}
-	
+
 	public String getDeparturedate() {
 		return departuredate;
 	}
-	
+
 	public void setDeparturedate(String departuredate) {
 		this.departuredate = departuredate;
 	}
-	
+
 	public String getDeparturetime() {
 		return departuretime;
 	}
-	
+
 	public void setDeparturetime(String departuretime) {
 		this.departuretime = departuretime;
 	}
-	
+
 	public String getArrivaldate() {
 		return arrivaldate;
 	}
-	
+
 	public void setArrivaldate(String arrivaldate) {
 		this.arrivaldate = arrivaldate;
 	}
-	
+
 	public String getArrivaltime() {
 		return arrivaltime;
 	}
-	
+
 	public void setArrivaltime(String arrivaltime) {
 		this.arrivaltime = arrivaltime;
 	}
-	
+
 	public int getSeats() {
 		return seats;
 	}
-	
+
 	public void setSeats(int seats) {
 		this.seats = seats;
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	public Seats getSeatInfo() {
-		return this.flightseats;
+
+	public int getAvalible() {
+		return this.avalibleSeats;
+	}
+
+	public int getFirstClass() {
+		return this.firstClass;
+	}
+
+	public void updateFirstClass(int numSeats) {
+		if (firstClass - numSeats < 0) {
+			throw new IllegalArgumentException("No more First Class seats are avalible");
+		} else {
+			this.firstClass = this.firstClass - numSeats;
+			this.avalibleSeats = this.avalibleSeats - numSeats;
+		}
+	}
+
+	public int getEconomyClass() {
+		return this.economyClass;
+	}
+
+	public void updateEconomyClass(int numSeats) {
+		if (economyClass - numSeats < 0) {
+			throw new IllegalArgumentException("No more Economy Class seats are avalible");
+		} else {
+			this.economyClass = this.economyClass - numSeats;
+			this.avalibleSeats = this.avalibleSeats - numSeats;
+		}
 	}
 
 	@Override
 	public int compare(Object o1, Object o2) {
-		
+
 		return 0;
 	}
 }
